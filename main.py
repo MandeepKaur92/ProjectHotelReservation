047e15e36b8d98fa3f806bd12abece1e691a24c2
ManpreetSingh483 <84625414+ManpreetSingh483@users.noreply.github.com>
2021-07-30 15:12:42 -0400

    Add Booking in main.py

--- main.py
+++ main.py
@@ -89,7 +89,48 @@ def setContactDetials():
     return render_template('hotelIndex.html', message=message)
 
 
+@app.route('/getBookingDetials', methods=['GET', 'POST'])
+def getBookingDetials():
+    if request.method == 'POST':
+        firstname = request.form.get('firstname')
+        lastname = request.form.get('lastname')
+        country= request.form.get('country')
+        Email=request.form.get('Email')
+        PhoneNumber=request.form.get('PhoneNumber')
+        No_of_adult= request.form.get('No_of_adult')
+        No_of_kids = request.form.get('No_of_kids')
+        room = request.form.get('room')
+        typeroom = request.form.get('typeroom')
+        CheckIn = request.form.get('CheckIn')
+        CheckOut = request.form.get('CheckOut')
+        #result =
+        '''  <h1>First Name : {}<h1>
+                              <h1>Last Name : {}<h1> 
+                              <h1>country: {}<h1>
+                              <h1>email : {}<h1>
+                              <h1>tel: {}<h1>
+                              <h1>Adult : {}<h1>
+                              <h1> Kids : {}<h1>
+                              <h1> CheckIn : {}<h1>
+                              <h1> CheckOut : {}<h1>'''
 
+    #return result.format(firstname, lastname,country,Email,PhoneNumber,No_of_adult, No_of_kids,CheckIn,CheckOut)
+
+    db=connect_to_monogodb()
+    print(db.list_collection_names())
+    cBookingDetials = db["BookingDetials"]
+    result={'firstname':firstname,'lastname':lastname,'country':country,'Email':Email,'PhoneNumber':PhoneNumber,
+                            'No_of_adult':No_of_adult,'No_of_kids':No_of_kids,'No_of_rooms':room,'Room_Type':typeroom,'CheckIn':CheckIn,'CheckOut':CheckOut}
+    cBookingDetials.insert({'firstname':firstname,'lastname':lastname,'country':country,'Email':Email,'PhoneNumber':PhoneNumber,
+                            'No_of_adult':No_of_adult,'No_of_kids':No_of_kids,'No_of_rooms':room,'Room_Type':typeroom,'CheckIn':CheckIn,'CheckOut':CheckOut})
+
+    results = cBookingDetials.find()
+    print(results)
+    for row in results:
+        print(row)
+    message="Booking successfully complete"
+    #return render_template('hotelIndex.html',message=message)
+    return render_template('BookingMessage1.html', results=result)
 
 
 if __name__ == '__main__':
